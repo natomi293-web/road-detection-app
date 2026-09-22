0e6940746f05a08be592f6174e41325789541e30
natomi293-web <natomi293@gmail.com>
2026-09-22 09:41:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ PASSWORD = "DOURO12"  # ← 港さんが決めるパスワードに変更
 from roboflow import Roboflow
 
 rf = Roboflow(api_key="PNvjzdIq1rlRsoeBvXon")
-project = rf.workspace().project("new-workspace-nep6p/one-lane-road-detecter-2-rfdetr-small-t1")
+project = rf.workspace().project("new-workspace-nep6p/one-lane-road-detecter-2/rfdetr-small-t1")
 model = project.version(1).model
 
 @app.route("/login", methods=["GET", "POST"])
